3f23505b826812ba3fb96bd72da46861fb9b7a03
SzymCode <szymon.radomski@yahoo.com>
2026-03-03 22:12:43 +0100

    feat: add user profile settings

--- components/content/index.vue
+++ components/content/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="grided-content" v-if="gridedContent">
     <nuc-settings-preferences-theme v-if="route.hash === '#preferences-theme'" />
+    <nuc-users-profile-personal-info v-if="route.hash === '#my-profile-personal-info'" />
+    <nuc-users-profile-notifications-alerts v-if="route.hash === '#my-profile-notifications-alerts'" />
   </div> 
   <nuc-settings-staff-modules v-if="route.hash === '#modules'"/>
   <nuc-modules-settings-detail v-if="route.hash.includes('#module-') && !gridedContent">
@@ -11,12 +13,22 @@
 
 <script setup lang="ts">
 import { useRoute } from 'nuxt/app'
+import { computed } from 'vue'
+
+import {
+  NucUsersProfileNotificationsAlerts,
+  NucUsersProfilePersonalInfo,
+} from 'atomic'
 
 import { NucSettingsPreferencesTheme, NucSettingsStaffModules } from '.'
 
 const route = useRoute()
 
-const gridedHashes = ['#preferences-theme']
+const gridedHashes = [
+  '#preferences-theme',
+  '#my-profile-personal-info',
+  '#my-profile-notifications-alerts',
+]
 
 const gridedContent = computed(() => {
   return gridedHashes.includes(route.hash)
