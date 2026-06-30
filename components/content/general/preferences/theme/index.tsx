573950043186f2ba25445f9f05360603fea704d7
SzymCode <szymon.radomski@yahoo.com>
2026-07-01 02:46:12 +0700

    feat: add dock/sidebar options

--- components/content/general/preferences/theme/index.tsx
+++ components/content/general/preferences/theme/index.tsx
@@ -2,13 +2,21 @@
 
 import type { JSX } from 'react'
 
-import { NucColorSettingsCard, t } from 'nucleify'
+import {
+  NucChartSettingsCard,
+  NucColorSettingsCard,
+  NucDockSettingsCard,
+  t,
+} from 'nucleify'
 
 export function NucSettingsPreferencesTheme(): JSX.Element {
   return (
     <>
       <NucColorSettingsCard heading={t('settings-colors')} />
       <hr className="settings-card-divider" />
+      <NucChartSettingsCard heading={t('settings-charts')} />
+      <hr className="settings-card-divider" />
+      <NucDockSettingsCard heading={t('settings-dock-style')} />
     </>
   )
 }
--- components/content/general/preferences/theme/index.vue
+++ components/content/general/preferences/theme/index.vue
@@ -2,6 +2,8 @@
   <nuc-color-settings-card :heading="t('settings-colors')" />
   <hr class="settings-card-divider" />
   <nuc-chart-settings-card :heading="t('settings-charts')" />
+  <hr class="settings-card-divider" />
+  <nuc-dock-settings-card :heading="t('settings-dock-style')" />
 </template>
 
 <script setup lang="ts">
